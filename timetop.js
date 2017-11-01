ba59a5bdf9455b82574653d1936e977d162a5ef1
Haemin <31715504+xiaham@users.noreply.github.com>
2017-11-02 04:15:11 +0900

    Update and rename app.js to timetop.js

--- timetop.js
+++ timetop.js
@@ -5,7 +5,7 @@ const fps = 250;
 const lineWidth = 10;
 const lineSpacing = lineWidth / 2;
 
-const backgroundColour = "grey";
+const backgroundColour = "black";
 
 const colourGuides = "white";
 const colourHours = "cyan";
